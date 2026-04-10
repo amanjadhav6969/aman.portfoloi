a9d5273a16397b8319f9c0095fc64df8d292fd8a
amanjadhav6969 <amanjadhav6969@gmail.com>
2026-04-10 11:30:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling Navigation
+document.querySelectorAll("nav ul li a").forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
